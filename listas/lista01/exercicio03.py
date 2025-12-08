c7058f41c2fab3f98f0f5190bf671019dd8c3c2d
vicamanda <vitoriaamanda93651@gmail.com>
2025-12-08 16:01:06 -0300

    feat: adicionando o exercício 03 da lista 01

---
+++ listas/lista01/exercicio03.py
@@ -0,0 +1,7 @@
+# fórmula: idade x 365,25
+
+idade = float(input("Insira uma idade: "))
+
+dias = idade * 365,25
+
+print(f"A idade {idade} equivalente em dias, considerando também os anos bissextos é de {dias} dias.")
