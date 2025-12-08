14f41cc923fbb1f789a3e1d46abe76ff67ec719b
vicamanda <vitoriaamanda93651@gmail.com>
2025-12-08 16:50:05 -0300

    feat: adicionando exercício 05 da lista 01

---
+++ listas/lista01/exercicio05.py
@@ -0,0 +1,8 @@
+num = int(input("Insira um número: "))
+
+if num > 0:
+    print(f"O número {num} é positivo.")
+elif num < 0:
+    print(f"O número {num} é negativo.")
+else:
+    print(f"O número inserido foi zero.")
