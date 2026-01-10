17a8cc82e33b2b97b4b76df1467f39b9d54ef189
vicamanda <vitoriaamanda93651@gmail.com>
2026-01-10 17:54:26 -0300

    feat: adicionando exercicio 02 da lista 03

---
+++ listas/lista03/exercicio02.py
@@ -0,0 +1,10 @@
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+numeros = [5, 3, 8, 4, 2]
+bubble_sort(numeros)
+print(numeros)
